666bdb7ed78e3ffa38eeeef486f62a85bdf05e93
Alberto Casero <j.albertocg86@gmail.com>
2020-04-04 22:17:15 +0200

    Añadir apartados TG-10

--- components/Home.vue
+++ components/Home.vue
@@ -1,10 +1,12 @@
-<template id="home">
-  <b-container>
-    <h1 class="text-center">
-      Alberto Casero
-    </h1>
-    <h2 class="text-center">
-      Ingeniero Web
-    </h2>
-  </b-container>
+<template>
+  <div id="home">
+    <b-container>
+      <h1 class="text-center">
+        Alberto Casero
+      </h1>
+      <h2 class="text-center">
+        Ingeniero Web
+      </h2>
+    </b-container>
+  </div>
 </template>
---
+++ components/skills.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="skills">
+    <b-row>
+      <b-col>
+        <h1 class="text-center">
+          Conocimientos
+        </h1>
+      </b-col>
+    </b-row>
+  </div>
+</template>
