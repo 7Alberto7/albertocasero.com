2d877eee03c21a0d05484ced8090b0b533788c62
acaseromcr <a.casero@mcr.com.es>
2020-04-04 19:47:37 +0200

    Crear estilo base TG-9

---
+++ components/Home.vue
@@ -0,0 +1,10 @@
+<template id="home">
+  <b-container>
+    <h1 class="text-center">
+      Alberto Casero
+    </h1>
+    <h2 class="text-center">
+      Ingeniero Web
+    </h2>
+  </b-container>
+</template>
