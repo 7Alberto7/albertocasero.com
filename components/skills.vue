688720de36b7d9aa0c01ab563f8dfc738fc6eef5
Alberto Casero <j.albertocg86@gmail.com>
2020-05-01 14:39:25 +0200

    Crear componente de tarjeta TG-19

--- components/skills.vue
+++ components/skills.vue
@@ -1,9 +1,42 @@
 <template>
   <section id="skills">
-    <b-container>
-      <h1 class="section-title">
-        Conocimientos
-      </h1>
+    <b-container fluid>
+      <b-row class="pb-4">
+        <b-col>
+          <h1 class="section-title">
+            Conocimientos
+          </h1>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col
+          v-for="skill in skills"
+          :key="skill.name"
+          cols="12"
+          sm="4"
+          md="4"
+          lg="4"
+          xl="3"
+        >
+          <skill-card
+            :skill="skill"
+          />
+        </b-col>
+      </b-row>
     </b-container>
   </section>
 </template>
+<script>
+import SkillCard from '~/layouts/skillCard.vue'
+
+export default {
+  components: {
+    SkillCard
+  },
+  data () {
+    return {
+      skills: this.$info.skills
+    }
+  }
+}
+</script>
