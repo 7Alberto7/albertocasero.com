c2f9ac60f85ae6e358ff5379062e4dc2be6d44ee
acaseromcr <a.casero@mcr.com.es>
2020-05-01 14:39:25 +0200

    Crear componente de tarjeta TG-19

---
+++ layouts/skillCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-card
+    tag="article"
+    class="mb-3"
+    header-bg-variant="white"
+    header-text-variant="secondary"
+    body-bg-variant="secondary"
+    body-text-variant="white"
+    border-variant="secondary"
+  >
+    <template v-slot:header>
+      <b-card-title class="text-center">
+        <b-img :src="'/img/skills/'+skill.image" fluid :alt="skill.name" />
+        <span class="ml-2">{{ skill.name.toUpperCase() }}</span>
+      </b-card-title>
+    </template>
+    <b-card-body>
+      <b-card-text>
+        {{ skill.description }}
+      </b-card-text>
+      <b-form-rating v-model="skill.level_of_knowledge" class="bg-white" variant="warning" readonly />
+    </b-card-body>
+  </b-card>
+</template>
+<script>
+export default {
+  props: {
+    skill: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
