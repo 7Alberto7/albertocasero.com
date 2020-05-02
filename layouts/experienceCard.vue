e50979925ddb39cd69259fc44302dce9fff1dbef
Alberto Casero <j.albertocg86@gmail.com>
2020-05-02 13:24:06 +0200

    Crear el componente de experiencia TG-26

---
+++ layouts/experienceCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-card
+    class="mb-3"
+    no-body
+  >
+    <b-card-body>
+      <b-card-title>
+        {{ experience.position }}
+        <b-badge
+          class="float-right"
+          variant="primary"
+        >
+          {{ experience.begin_at + ' - ' + (experience.end_at != null ? experience.end_at : 'Actualmente') }}
+        </b-badge>
+      </b-card-title>
+      <b-card-sub-title>
+        {{ experience.company }}
+      </b-card-sub-title>
+      <b-card-text>
+        {{ experience.description }}
+      </b-card-text>
+    </b-card-body>
+  </b-card>
+</template>
+<script>
+export default {
+  props: {
+    experience: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- plugins/load-info.js
+++ plugins/load-info.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import presentation from '~/static/json/presentation.json'
 import projects from '~/static/json/projects.json'
 import skills from '~/static/json/skills.json'
+import experience from '~/static/json/work_experience.json'
 
 Vue.prototype.$info = {
   presentation,
   projects: projects.sort((a, b) => b.relevance - a.relevance),
-  skills: skills.sort((a, b) => ((b.currently * 10) + b.level_of_knowledge) - ((a.currently * 10) + a.level_of_knowledge))
+  skills: skills.sort((a, b) => ((b.currently * 10) + b.level_of_knowledge) - ((a.currently * 10) + a.level_of_knowledge)),
+  experience
 }
