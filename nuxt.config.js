86c9674d542f7677b6db76367ce2a617f88629ca
acaseromcr <a.casero@mcr.com.es>
2020-05-23 12:34:47 +0200

    Incluir posicionamiento SEO TG-58

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,20 @@
 
 module.exports = {
-  mode: 'spa',
+  mode: 'universal',
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Alberto Casero',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Soy Alberto Casero, máster en ingeniería web por la Universidad Politécnica de Madrid, con varios años de experiencia en gestión de proyectos y programación tanto en el lado cliente como en el servidor.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
     ]
   },
   /*
@@ -37,7 +39,13 @@ module.exports = {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/dotenv',
+    ['@nuxtjs/google-analytics', {
+      id: process.env.google_analytics_id,
+      dev: false
+    }],
+    '@nuxtjs/sitemap'
   ],
   /*
   ** Nuxt.js modules
@@ -73,6 +81,26 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
-    }
+    },
+    transpile: [
+      'vue-fab',
+      'vue-ripple-directive'
+    ]
+  },
+  /*
+  ** Sitemap configuration
+  */
+  sitemap: {
+    hostname: process.env.hostname,
+    cacheTime: 1000 * 60 * 15,
+    gzip: true,
+    routes: [
+      '/'
+    ].map(route => ({
+      url: route,
+      changefreq: 'monthly',
+      priority: 1,
+      lastmodISO: new Date().toISOString().split('T')[0]
+    }))
   }
 }
